3356a0f8dbacbeaa57508b68604afa40b259566b
wuhaipeng84 <candy@WUHP.com>
2012-06-21 14:32:19 +0800

    add person entity

---
+++ hibernate-tutorial/src/main/java/com/hibernate/tutorial/domain/Person.java
@@ -0,0 +1,80 @@
+package com.hibernate.tutorial.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Person {
+
+	@Id
+	private Long id;
+	private String firstName;
+	private String lastName;
+	
+	@OneToMany
+	private Set<Event> events = new HashSet<Event>();
+	
+	public Person() {}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * @return the events
+	 */
+	public Set<Event> getEvents() {
+		return events;
+	}
+
+	/**
+	 * @param events the events to set
+	 */
+	public void setEvents(Set<Event> events) {
+		this.events = events;
+	}
+
+	
+}
